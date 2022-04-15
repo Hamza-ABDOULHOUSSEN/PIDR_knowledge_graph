185227460755af21e4a17283e0f65ecbe7fcfeb7
Hamza-ABDOULHOUSSEN <abd.hamza.ha@gmail.com>
2022-04-15 14:50:54 +0200

    Add tests

--- python/test/test_extractor.py
+++ python/test/test_extractor.py
@@ -1,9 +1,22 @@
 import pytest
 from OwlToPythonClass.extractor import *
+import os
 
 ONTOLOGY = "file://resource/Chess_Ontology.owl"
 
-# load the ontology
+BASE = "file://resource/"
+
+# searching for all ontologies
+ALL_ONTOLOGIES = os.listdir("resource")
+for ELEM in ALL_ONTOLOGIES:
+    if not(ELEM[-4:] == ".owl"):
+        ALL_ONTOLOGIES.remove(ELEM)
+
+### load the ontology
+# removing previous ontologies loaded
+for ONTO in ALL_ONTOLOGIES:
+    get_ontology(BASE+ONTO).destroy()
+
 get_ontology(ONTOLOGY).load()
 
 chess = graph(name="chess")
